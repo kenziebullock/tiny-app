da79396a9779b8765f392d4e980ff2cdcec4a1a9
Kenzie Bullock <kenzieb_13@shaw.ca>
2018-09-04 23:59:41 +0000

    added a few simple functions that are working with a static database of URLs for testing purposes

---
+++ express-server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.set('view engine', 'ejs');
+
+const urlDatabase = {
+    'b2xVn2': 'http://www.lighthouselabs.ca',
+    '9sm5xK': 'http://www.google.com'
+};
+
+app.get('/urls', (request, respond) => {
+    let templateVars = { urls: urlDatabase };
+    respond.render('urls-index', templateVars);
+});
+
+app.get('/urls/:id', (request, respond) => {
+    let templateVars = { shortURL: request.params.id, longURL: urlDatabase };
+    respond.render('urls-show', templateVars);
+});
+
+app.get('/', (request, respond) => {
+    respond.send('Hello!');
+});
+
+app.get('/urls.json', (request, respond) => {
+    respond.json(urlDatabase);
+});
+
+app.get('/hello', (request, respond) => {
+    respond.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+});

f8b9ec3c34bd4699c184da666cadd75933e0239f
Kenzie Bullock <kenzieb_13@shaw.ca>
2018-09-05 01:18:57 +0000

    adding post functionality to server, providing random string for website entered and writing to variable

--- express-server.js
+++ express-server.js
@@ -1,44 +1,59 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
-const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
-const urlDatabase = {
+let urlDatabase = {
     'b2xVn2': 'http://www.lighthouselabs.ca',
-    '9sm5xK': 'http://www.google.com'
+    '9sm5xK': 'http://www.google.com',
+    'dx5gt2': 'http://www.reddit.com'
 };
 
-app.get('/urls', (request, respond) => {
+app.get('/urls', (request, response) => {
     let templateVars = { urls: urlDatabase };
-    respond.render('urls-index', templateVars);
+    response.render('urls-index', templateVars);
 });
 
-app.get('/urls/:id', (request, respond) => {
+app.get('/urls/new', (request, response) => {
+    response.render('urls-new');
+});
+
+app.get('/urls/:id', (request, response) => {
     let templateVars = { shortURL: request.params.id, longURL: urlDatabase };
-    respond.render('urls-show', templateVars);
+    response.render('urls-show', templateVars);
 });
 
-app.get('/', (request, respond) => {
-    respond.send('Hello!');
+app.get('/', (request, response) => {
+    response.send('Hello!');
 });
 
-app.get('/urls.json', (request, respond) => {
-    respond.json(urlDatabase);
+app.get('/urls.json', (request, response) => {
+    response.json(urlDatabase);
 });
 
-app.get('/urls/new', (request, respond) => {
-    res.render('urls-new');
+app.get('/hello', (request, response) => {
+    response.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-app.get('/hello', (request, respond) => {
-    respond.send('<html><body>Hello <b>World</b></body></html>\n');
-});
-
-
+app.post('/urls', (request, response) => {
+    console.log(request.body.longURL);
+    urlDatabase[generateRandomString()] = request.body.longURL;
+    console.log(urlDatabase);
+    // response
+    //response.send('generateRandomString()');
+    //response.writeHead(301, )
+    
+})
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+    let rndString = '';
+    rndString = Math.random().toString(36).substr(2, 6);
+    return rndString;
+}
